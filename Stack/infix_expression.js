a925ce8fada58e688eb710e04de8bc1c8800aa19
Suraj Singh <playerking2244@gmail.com>
2026-03-26 09:49:22 +0530

    stack infix-expression

---
+++ Stack/infix_expression.js
@@ -0,0 +1,43 @@
+let str = '9-5+3*4/6';
+console.log(str);
+let val = [];
+let op = [];
+for (let i = 0; i < str.length; i++) {
+    let ch = str[i];
+
+    if (ch>= '0'&& ch <='9') {
+        val.push(Number(ch));
+    }else if (op.length==0) {
+        op.push(ch);
+    }else{
+        if (ch=='+' || ch=='-') {
+            let v2 = val.pop();
+            let v1 = val.pop();
+            if(op[op.length-1]=='-') val.push(v1-v2);
+            if(op[op.length-1]=='+') val.push(v1+v2);
+            // if(op[op.length-1]=='*') val.push(v1*v2);
+            // if(op[op.length-1]=='/') val.push(v1/v2);
+            op.pop();
+            op.push(ch);
+        }else if (ch=='*' || ch=='/') {
+            if (op[op.length-1]=='*' || op[op.length-1]=='/') {
+                let v2 = val.pop();
+                let v1 = val.pop();
+                if(op[op.length-1]=='*') val.push(v1*v2);
+                if(op[op.length-1]=='/') val.push(v1/v2);
+                op.pop();
+                op.push(ch);
+            }else op.push(ch);
+        }
+    }
+}
+while (op.length>0) {
+            let v2 = val.pop();
+            let v1 = val.pop();
+            if(op[op.length-1]=='-') val.push(v1-v2);
+            if(op[op.length-1]=='+') val.push(v1+v2);
+            if(op[op.length-1]=='*') val.push(v1*v2);
+            if(op[op.length-1]=='/') val.push(v1/v2);
+            op.pop();
+}
+console.log(val[val.length-1]);
